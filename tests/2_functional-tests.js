09c8c2ec78daa9fefc8c7983f1aae5d54cbb1b48
SaintPeter <rex.schrader@gmail.com>
2020-09-18 06:51:52 -0700

    Fix unit case

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -26,7 +26,7 @@ suite('Functional Tests', function() {
         .end(function(err, res){
           assert.equal(res.status, 200);
           assert.equal(res.body.initNum, 10);
-          assert.equal(res.body.initUnit, 'L');
+          assert.equal(res.body.initUnit, 'l');
           assert.approximately(res.body.returnNum, 2.64172, 0.1);
           assert.equal(res.body.returnUnit, 'gal');
           done();
