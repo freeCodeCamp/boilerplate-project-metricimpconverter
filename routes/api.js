7fab5e7fd050777d47582a8c02a6d40d826385ef
Nicholas Carrigan (he/him) <nhcarrigan@gmail.com>
2021-03-12 08:39:21 -0800

    Remove tests from boilerplate (#14)
    * Remove tests from boilerplate
    
    Signed-off-by: nhcarrigan <nhcarrigan@gmail.com>
    
    * fix: Comment out NODE_ENV
    
    Signed-off-by: nhcarrigan <nhcarrigan@gmail.com>

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,3 @@
-/*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
-
 'use strict';
 
 const expect = require('chai').expect;
@@ -15,16 +7,4 @@ module.exports = function (app) {
   
   let convertHandler = new ConvertHandler();
 
-  app.route('/api/convert')
-    .get(function (req, res){
-      let input = req.query.input;
-      let initNum = convertHandler.getNum(input);
-      let initUnit = convertHandler.getUnit(input);
-      let returnNum = convertHandler.convert(initNum, initUnit);
-      let returnUnit = convertHandler.getReturnUnit(initUnit);
-      let toString = convertHandler.getString(initNum, initUnit, returnNum, returnUnit);
-      
-      //res.json
-    });
-    
 };
